eb79818290390eebee74fd50dcb752293eca9205
EmmanuelManalo <emmanuel.manalo@frontlinebusiness.com.ph>
2023-09-26 13:54:48 +0800

    update eman

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Test from "./components/pages/Test";
+
 function App() {
-  return <></>;
+  const queryClient = new QueryClient();
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <StoreProvider>
+          <Router>
+            <Routes>
+              <Route path={"*"} element={<Test />} />
+              <Route path={"/product"} element={<Test />} />
+              <Route path={"/individual*"} element={<Test />} />
+              <Route path={"/transaction"} element={<Test />} />
+              <Route path={"/checkout"} element={<Test />} />
+            </Routes>
+          </Router>
+        </StoreProvider>
+      </QueryClientProvider>
+    </>
+  );
 }
 
 export default App;
