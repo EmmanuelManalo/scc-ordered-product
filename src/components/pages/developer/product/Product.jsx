f4d34175a2c3c9342fa3347551e81bddc21b4f0c
EmmanuelManalo <emmanuel.manalo@frontlinebusiness.com.ph>
2023-09-27 09:43:29 +0800

    update eman

--- src/components/pages/developer/product/Product.jsx
+++ src/components/pages/developer/product/Product.jsx
@@ -25,7 +25,7 @@ const Product = () => {
         >
           <Navigation menu={`product`} />
         </aside>
-        <main className="p-3 !pb-6 lg:p-0 lg:pr-10 custom__scroll">
+        <main className="px-2 lg:p-0 lg:pr-10 custom__scroll">
           <div className="flex items-center justify-between py-3 pt-6">
             <h1 className="mb-0">Product</h1>
             <button className="btn btn-primary btn--sm" onClick={handleAdd}>
