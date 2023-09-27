dd2112ee14a2c37a11a7ea5fa11dcce22d951657
EmmanuelManalo <emmanuel.manalo@frontlinebusiness.com.ph>
2023-09-27 09:58:55 +0800

    update

--- src/components/pages/developer/transaction/Transaction.jsx
+++ src/components/pages/developer/transaction/Transaction.jsx
@@ -1,20 +1,16 @@
 import React from "react";
 import { StoreContext } from "../../../../store/StoreContext";
-import { setIsAdd } from "../../../../store/StoreAction";
 import Header from "../../../partials/Header";
 import Navigation from "../../../partials/Navigation";
-import TransactionTable from "./TransactionTable";
-import ModalAddTransaction from "./ModalAddTransaction";
-import ModalValidate from "../../../partials/modals/ModalValidate";
 import Toast from "../../../partials/Toast";
+import ModalValidate from "../../../partials/modals/ModalValidate";
+import ModalAddTransaction from "./ModalAddTransaction";
+import TransactionTable from "./TransactionTable";
 
 const Transaction = () => {
-  const { store, dispatch } = React.useContext(StoreContext);
+  const { store } = React.useContext(StoreContext);
   const [itemEdit, setItemEdit] = React.useState(null);
-  const handleAdd = () => {
-    setItemEdit(null);
-    dispatch(setIsAdd(true));
-  };
+
   return (
     <>
       <Header />
@@ -27,9 +23,6 @@ const Transaction = () => {
         <main className="px-2 lg:pr-10 custom__scroll">
           <div className="flex items-center justify-between my-5">
             <h1 className="mb-0">Transaction</h1>
-            <button className="btn btn-primary btn--sm" onClick={handleAdd}>
-              Add
-            </button>
           </div>
           <div>
             <TransactionTable setItemEdit={setItemEdit} />
